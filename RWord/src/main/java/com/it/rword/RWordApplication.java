9b613255548043286ff501f539dd6d349bc198d6
voyage <voyage@qq.com>
2023-04-08 09:03:18 +0800

    commit01

---
+++ RWord/src/main/java/com/it/rword/RWordApplication.java
@@ -0,0 +1,13 @@
+package com.it.rword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RWordApplication.class, args);
+    }
+
+}
---
+++ RWord/src/test/java/com/it/rword/RWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.rword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
